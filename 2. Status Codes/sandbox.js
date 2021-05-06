1e67862010cd79bf45b9858440dc9674d487741b
ComputerScientist-01 <rathi406@gmail.com>
2021-05-06 18:49:39 +0530

    adding comments

--- 2. Status Codes/sandbox.js
+++ 2. Status Codes/sandbox.js
@@ -1,7 +1,6 @@
 const request = new XMLHttpRequest();
 
 request.addEventListener("readystatechange", () => {
-	//console.log(request, request.readyState);
 	if (request.readyState === 4 && request.status === 200) {
 		console.log(request.responseText);
 	} else if (request.readyState === 4) {
