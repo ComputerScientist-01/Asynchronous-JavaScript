50fdd7838bef3de3147a2bbc78f03ec477f4de94
ComputerScientist-01 <rathi406@gmail.com>
2021-05-06 15:44:51 +0530

    added section 2

---
+++ 2. Status Codes/sandbox.js
@@ -0,0 +1,14 @@
+const request = new XMLHttpRequest();
+
+request.addEventListener("readystatechange", () => {
+	//console.log(request, request.readyState);
+	if (request.readyState === 4 && request.status === 200) {
+		console.log(request.responseText);
+	} else if (request.readyState === 4) {
+		console.log("could not fetch the data");
+		//since the API end point doesn't exist we are logging an error message
+	}
+});
+
+request.open("GET", "https://jsonplaceholder.typicode.com/todoss");
+request.send();
