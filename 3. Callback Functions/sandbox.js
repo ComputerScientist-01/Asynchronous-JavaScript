9e7544c7976feb04c8d9018d9df84cb691c2202d
ComputerScientist-01 <rathi406@gmail.com>
2021-05-06 18:50:15 +0530

    added section 3

---
+++ 3. Callback Functions/sandbox.js
@@ -0,0 +1,32 @@
+const getTodos = (callback) => {
+	const request = new XMLHttpRequest();
+
+	request.addEventListener("readystatechange", () => {
+		if (request.readyState === 4 && request.status === 200) {
+			callback(undefined, request.responseText);
+		} else if (request.readyState === 4) {
+			callback("couldn't fetch data", undefined);
+		}
+	});
+
+	request.open("GET", "https://jsonplaceholder.typicode.com/todoss/");
+	request.send();
+};
+
+console.log(1);
+console.log(2);
+getTodos((err, data) => {
+	console.log("callback fired !");
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
+	}
+});
+// only the err & data messages will be printed
+//  & the undefined one's wont be
+console.log(3);
+console.log(4);
+
+//now to check the asnycronous nature we will log some statements and see the result.
+//since getTodos is running last we can safely say it is a non blocking function
