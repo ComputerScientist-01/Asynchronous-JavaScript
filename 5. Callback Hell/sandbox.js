0c0444b5dd7a9a4b07868d417040e4af1160c572
ComputerScientist-01 <rathi406@gmail.com>
2021-05-09 15:29:39 +0530

    adding section 5

---
+++ 5. Callback Hell/sandbox.js
@@ -0,0 +1,26 @@
+const getTodos = (resource, callback) => {
+	const request = new XMLHttpRequest();
+
+	request.addEventListener("readystatechange", () => {
+		if (request.readyState === 4 && request.status === 200) {
+			const data = JSON.parse(request.responseText);
+			callback(undefined, data);
+		} else if (request.readyState === 4) {
+			callback("couldn't fetch data", undefined);
+		}
+	});
+	request.open("GET", resource);
+	request.send();
+};
+
+getTodos("json/luigi.json", (err, data) => {
+	console.log(data);
+	getTodos("json/mario.json", (err, data) => {
+		console.log(data);
+		getTodos("json/shaun.json", (err, data) => {
+			console.log(data);
+		});
+	});
+});
+
+// nesting callbacks within callbacks is called callback hell
